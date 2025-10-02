f31ce93a91d7267cd299d053bbb76567aa7ca875
ME-AT <meatish2008@gmail.com>
2025-10-02 20:44:33 +0330

    feat(redis.module.ts): set up redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppDataSource } from './database/data-source.js';
 import { BookModule } from './modules/book/book.module.js';
 import { AuthModule } from './modules/auth/auth.module.js';
 import { TokensModule } from './modules/tokens/token.module.js';
+import { RedisModule } from './modules/redis/redis.module.js';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { TokensModule } from './modules/tokens/token.module.js';
     BookModule,
     AuthModule,
     TokensModule,
+    RedisModule,
   ],
   controllers: [],
   providers: [],

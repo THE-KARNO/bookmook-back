ee735ec4eba4dbf46c7bd6fd74fdbea2e86a9609
ME-AT <meatish2008@gmail.com>
2025-10-03 17:02:04 +0330

    feat(book.module.ts): register multer

--- src/modules/book/book.module.ts
+++ src/modules/book/book.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BookService } from './book.service.js';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { MulterModule } from '@nestjs/platform-express';
 
+import { BookService } from './book.service.js';
 import { BookController } from './book.controller.js';
 import { Book } from './book.entity.js';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book])],
+  imports: [
+    TypeOrmModule.forFeature([Book]),
+
+    MulterModule.register({
+      storage: diskStorage({
+        destination: '../../../uploads',
+        filename: (req, file, cb) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          cb(null, uniqueSuffix + extname(file.originalname));
+        },
+      }),
+    }),
+  ],
   controllers: [BookController],
   providers: [BookService],
 })
